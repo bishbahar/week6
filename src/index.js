75f2e715542792473a89893affb07b3b1f10c3f9
Narges <bish_bahar@yahoo.com>
2023-01-22 18:54:19 +0330

    improve sunrise and sunset conversion

--- src/index.js
+++ src/index.js
@@ -24,44 +24,18 @@ let maxTemp = document.querySelector("#temp-max");
 let minTemp = document.querySelector("#temp-low");
 let speedWind = document.querySelector("#speed-wind");
 let humidity = document.querySelector("#humidity");
+let sunrise=document.querySelector("#sunrise");
+let sunset=document.querySelector("#sunset");
 
-function formatDay(timestamp){
-  let date=new Date(timestamp*1000);
-  let day=date.getDay();
-  let days=["Sun","Mon", "Tue","Wed","Thu","Fri","Sat"];
 
-  return days[day];
-}
-function showForecast(repsonse){
-let forecastDaily=response.data.daily;
-let forecast=document.querySelector(".forecast");
-forecastDaily.forEach(function (days){
-  forecast.innerHTML=`<div class="row">
-          <div class="col-2 forecast-frame">
-            <div class="day-forecast">${formatDay(days.dt)}</div>
-            <img src="https://openweathermap.org/img/wn/${days.weather[0].icon}@2x.png">
-            <div class="temp-forecat">
-              <span class="max-temp">${Math.round(days.temp.max)}°</span>
-              <span class="min-temp">${Math.round(days.temp.min)}°</span>
-            </div>
-          </div>
-        </div>
-        `;
-})
-}
 
-function getForecast(coordinates){
-  let url = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}`;
-  console.log(url);
-  axios.get(url).then(showForecast);
-  
-}
 
 //current
 function showWeather(response) {
   degree.innerHTML = Math.round(response.data.main.temp);
   city.innerHTML = response.data.name;
   description.innerHTML = response.data.weather[0].description;
+  
 }
 
 function showPositin(position) {
@@ -81,7 +55,7 @@ serach.addEventListener("click", function (event) {
   city.innerHTML = searchInput.value;
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(function (response) {
-    console.log(response.data);
+    
     currentdegree.innerHTML = Math.round(response.data.main.temp);
     description.innerHTML = response.data.weather[0].description;
     icon.setAttribute(
@@ -93,6 +67,15 @@ serach.addEventListener("click", function (event) {
     minTemp.innerHTML = Math.round(response.data.main.temp_min);
     speedWind.innerHTML=Math.round(response.data.wind.speed);
     humidity.innerHTML=response.data.main.humidity;
+
+    //sunset and sunrise conversion
+    let timezone = response.data.main.timezone;
+    let sunriseData = response.data.sys.sunrise;
+    let sunsetData = response.data.sys.sunset;
+
+    sunrise.innerHTML=moment.utc(sunriseData,'X').add(timezone,'seconds').format('HH:mm a');
+    sunset.innerHTML=moment.utc(sunsetData,'X').add(timezone,'seconds').format('HH:mm a');
+
     let fahrenheit = document.querySelector("#fahrenheit");
     fahrenheit.addEventListener("click", function () {
       let fahrenheitTemperature = Math.round(
@@ -106,6 +89,7 @@ serach.addEventListener("click", function (event) {
       currentdegree.innerHTML = Math.round(response.data.main.temp);
     });
 
-    getForecast(response.data.coord);
   });
 });
+
+
